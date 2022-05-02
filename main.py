1d31219f4e5e172358cedcb336c82addc754b3f2
kderune <91871014+xushabanana0123@users.noreply.github.com>
2022-05-02 07:09:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+a=input('Ievadiet veselu skaitli:')
+b=input('Ievadiet veselu skaitli:')
+if a==b:
+  print('Skaitli ir vienādi')
+else:
+  if a>b:
+    print('Lielāks skaitlis:',a)
+  else:
+    if b>a:
+      print('Lielāks skaitlis:',b)
